f48db8316a150341b22848cd9c3fa406cce3b51f
Shay Ellis <58398990+ShayEllis@users.noreply.github.com>
2023-03-24 12:55:07 -0700

    cleaned up Field class and corrected a couple movement bugs

--- helpers/generateField.js
+++ helpers/generateField.js
@@ -1,29 +1,30 @@
-const field = [
-    ['*', 'O', '░', '░', '░', '░', '░'],
-    ['░', 'O', '░', 'O', 'O', '░', '░'],
-    ['░', 'O', '░', '░', 'O', 'O', '░'],
-    ['░', '░', '░', 'O', '░', '░', '░'],
-    ['O', '░', 'O', '░', 'O', 'O', 'O'],
-    ['░', '░', '░', '░', '░', '░', '░'],
-    ['O', 'O', 'O', 'O', 'O', 'O', '░'],
-    ['O', '░', 'O', '░', '░', '░', '░'],
-    ['░', '░', '░', '░', 'O', 'O', 'O'],
-    ['░', 'O', 'O', 'O', 'O', 'O', 'O'],
-    ['░', '░', '░', '░', 'O', '^', 'O'],
-    ['O', '░', 'O', '░', '░', '░', 'O'],
-]
-
 class Field {
     #field
     #pathCharacterIndex
     #hatIndex
     constructor(field) {
-        this.#field = field
+        this.#field = this.generateField()
         this.pathCharacter = '*'
         this.hatCharacter = '^'
         this.holeCharacter = 'O'
         this.fieldCharacter = '░'
         this.#initializeFieldProperties()
+    }
+    static generateField() {
+        return [
+            ['*', 'O', '░', '░', '░', '░', '░'],
+            ['░', 'O', '░', 'O', 'O', '░', '░'],
+            ['░', 'O', '░', '░', 'O', 'O', '░'],
+            ['░', '░', '░', 'O', '░', '░', '░'],
+            ['O', '░', 'O', '░', 'O', 'O', 'O'],
+            ['░', '░', '░', '░', '░', '░', '░'],
+            ['O', 'O', 'O', 'O', 'O', 'O', '░'],
+            ['O', '░', 'O', '░', '░', '░', '░'],
+            ['░', '░', '░', '░', 'O', 'O', 'O'],
+            ['░', 'O', 'O', 'O', 'O', 'O', 'O'],
+            ['░', '░', '░', '░', 'O', '^', 'O'],
+            ['O', '░', 'O', '░', '░', '░', 'O'],
+        ]
     }
     #initializeFieldProperties() {
         for (let i = 0; i < this.#field.length - 1; i++) {
@@ -36,6 +37,18 @@ class Field {
             }
         }
     }
+    #checkLocation(index) {
+        if (index[0] < 0 || index[1] < 0) {
+            return 'Game Over! You moved outside of the field!'
+        } else if (this.#field[index[0]][index[1]] === this.holeCharacter) {
+            return 'Game Over! You fell in a hole!'
+        } else if (this.#field[index[0]][index[1]] === this.hatCharacter) {
+            return 'You Won! You found the hat!'
+        } else {
+            this.#movePathCharacter(index)
+            return true
+        }
+    }
     #movePathCharacter(index) {
         this.#field[this.#pathCharacterIndex[0]][this.#pathCharacterIndex[1]] = this.fieldCharacter
         this.#field[index[0]][index[1]] = this.pathCharacter
@@ -45,24 +58,25 @@ class Field {
         const stringField = this.#field.map((row) => row.join('')).join('\n')
         return stringField
     }
-    moveUp() {
-        const newIndex = [this.#pathCharacterIndex[0] - 1, this.#pathCharacterIndex[1]]
-        //check for negative numbers - out of field
-        //check for hole - hole character - fell in hole
-        //Found hat - won
-        this.#movePathCharacter(newIndex)
-    }
-    moveDown() {
-        const newIndex = [this.#pathCharacterIndex[0] + 1, this.#pathCharacterIndex[1]]
-        this.#movePathCharacter(newIndex)
-    }
-    moveLeft() {
-        const newIndex = [this.#pathCharacterIndex[0], this.#pathCharacterIndex[1] - 1]
-        this.#movePathCharacter(newIndex)
-    }
-    moveRight() {
-        const newIndex = [this.#pathCharacterIndex[0], this.#pathCharacterIndex[1] + 1]
-        this.#movePathCharacter(newIndex)
+    move(direction) {
+        switch (direction.toLowerCase()) {
+            case 'u': {
+                const newIndex = [this.#pathCharacterIndex[0] - 1, this.#pathCharacterIndex[1]]
+                return this.#checkLocation(newIndex)
+            }
+            case 'd': {
+                const newIndex = [this.#pathCharacterIndex[0] + 1, this.#pathCharacterIndex[1]]
+                return this.#checkLocation(newIndex) 
+            }
+            case 'l': {
+                const newIndex = [this.#pathCharacterIndex[0], this.#pathCharacterIndex[1] - 1]
+                return this.#checkLocation(newIndex) 
+            }
+            case 'r': {
+                const newIndex = [this.#pathCharacterIndex[0], this.#pathCharacterIndex[1] + 1]
+                return this.#checkLocation(newIndex) 
+            }
+        }
     }
 }
 
--- main.js
+++ main.js
@@ -2,28 +2,14 @@ import { getUserInput } from "./helpers/functions.js";
 import Field, { field } from "./helpers/generateField.js";
 
 const gameField = new Field(field)
+let moveResult = true
 
-while (true) {
+while (moveResult === true) {
     console.log(gameField.print())
 
     const direction = getUserInput()
 
-    switch (direction.toLowerCase()) {
-        case 'u': {
-            gameField.moveUp()
-            break
-        }
-        case 'd': {
-            gameField.moveDown()
-            break        
-        }
-        case 'l': {
-            gameField.moveLeft()
-            break
-        }
-        case 'r': {
-            gameField.moveRight()
-            break
-        }
-    }
+    moveResult = gameField.move(direction)
 }
+
+console.log(moveResult)
